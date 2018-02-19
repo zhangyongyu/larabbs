dd24180e000c9b43b64f75aa08c95c2104fc430b
Zhangyongyu <zhangyongyu1024@gmail.com>
2018-02-19 16:25:54 +0000

    Seeding topics and users data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
 		$this->call(TopicsTableSeeder::class);
     }
 }
